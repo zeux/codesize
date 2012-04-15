243d01bd24a01c5d6564875fd9f26afd356b2702
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-04-15 12:45:51 -0700

    Use command line argument or open file dialog instead of hardcoded ELF path

--- src/codesize.fs
+++ src/codesize.fs
@@ -2,6 +2,7 @@ open System
 open System.Windows
 open System.Windows.Controls
 open System.Windows.Threading
+open Microsoft.Win32
 
 open Symbols
 
@@ -12,18 +13,30 @@ let config =
      "tools/c++filt/@path", @"D:\work\codesize\tools\c++filt.exe"]
     |> Map.ofList
 
-let path = @"D:\work\codesizeelf\test.elf"
-
-let t = TreeView(ItemsSource = [sprintf "Loading %s..." path])
+let t = TreeView()
 let w = Window(Content = t)
 let app = Application()
 let context = DispatcherSynchronizationContext(app.Dispatcher)
 
-async {
-    let ess = ElfSymbolSource(path, fun key -> Map.find key config) :> ISymbolSource
-    let symbols = ess.Symbols |> Seq.map (fun s -> int s.size, s.section, s.name) |> Seq.toArray
-    do! Async.SwitchToContext context
-    TreeView.bindToView t symbols
-} |> Async.Start
+app.Activated.Add(fun _ ->
+    let path =
+        if Environment.GetCommandLineArgs().Length > 1 then
+            Environment.GetCommandLineArgs().[1]
+        else
+            let dlg = OpenFileDialog(DefaultExt = ".elf", Filter = "ELF files (.elf)|*.elf")
+            let res = dlg.ShowDialog(w)
+            if res.HasValue && res.Value then
+                dlg.FileName
+            else
+                exit 0
+
+    t.ItemsSource <- [sprintf "Loading %s..." path]
+
+    async {
+        let ess = ElfSymbolSource(path, fun key -> Map.find key config) :> ISymbolSource
+        let symbols = ess.Symbols |> Seq.map (fun s -> int s.size, s.section, s.name) |> Seq.toArray
+        do! Async.SwitchToContext context
+        TreeView.bindToView t symbols
+    } |> Async.Start)
 
 app.Run(w) |> ignore
