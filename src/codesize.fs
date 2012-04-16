3e84398a6acb0ee6f83e4bcf4da52fdf6fca096a
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-04-15 19:55:21 -0700

    Added loading progress bar, moved all controls to controls module

--- src/codesize.fs
+++ src/codesize.fs
@@ -17,18 +17,23 @@ let config =
      "tools/demangle/@path", exepath + @"\demangle.exe"]
     |> Map.ofList
 
-let w = Application.LoadComponent(Uri("src/ui/mainwindow.xaml", UriKind.Relative)) :?> Window
+let window = Application.LoadComponent(Uri("src/ui/mainwindow.xaml", UriKind.Relative)) :?> Window
 
-let treeView = w?TreeView :?> TreeView
-let filterText = w?FilterText :?> TextBox
-let filterTextType = w?FilterTextType :?> ComboBox 
-let filterSize = w?FilterSize :?> TextBox
-let filterSections = w?FilterSections :?> ComboBox
+module controls =
+    let treeView = window?TreeView :?> TreeView
+    let filterText = window?FilterText :?> TextBox
+    let filterTextType = window?FilterTextType :?> ComboBox 
+    let filterSize = window?FilterSize :?> TextBox
+    let filterSections = window?FilterSections :?> ComboBox
+    let tabControl = window?TabControl :?> TabControl
+    let tabLoading = window?TabLoading :?> TabItem
+    let tabTreeView = window?TabTreeView :?> TabItem
+    let labelLoading = window?LabelLoading :?> Label
 
 let app = Application()
 let context = DispatcherSynchronizationContext(app.Dispatcher)
 
-let treeViewBinding = TreeView.Binding(treeView)
+let treeViewBinding = TreeView.Binding(controls.treeView)
 
 let getFilterText typ (text: string) =
     let contains (s: string) (p: string) = s.IndexOf(p, StringComparison.InvariantCultureIgnoreCase) >= 0
@@ -49,14 +54,14 @@ let getFilterText typ (text: string) =
         fun name -> true
 
 let getFilterSymbol () =
-    let ft = getFilterText filterTextType.SelectedIndex filterText.Text
+    let ft = getFilterText controls.filterTextType.SelectedIndex controls.filterText.Text
     let fs =
-        match Int32.TryParse(filterSize.Text) with
+        match Int32.TryParse(controls.filterSize.Text) with
         | true, limit -> fun size -> size >= limit
         | _ -> fun size -> true
     let fg =
         let sections =
-            filterSections.Items
+            controls.filterSections.Items
             |> Seq.cast<CheckBox>
             |> Seq.choose (fun cb -> if cb.IsChecked.Value then Some cb.Tag else None)
             |> Seq.cast<string>
@@ -86,20 +91,20 @@ let rebindToView syms =
 let bindToView syms =
     treeViewBinding.ItemsSource <- syms
 
-    filterText.TextChanged.Add(fun _ -> rebindToView syms)
-    filterTextType.SelectionChanged.Add(fun _ -> rebindToView syms)
-    filterSize.TextChanged.Add(fun _ -> rebindToView syms)
+    controls.filterText.TextChanged.Add(fun _ -> rebindToView syms)
+    controls.filterTextType.SelectionChanged.Add(fun _ -> rebindToView syms)
+    controls.filterSize.TextChanged.Add(fun _ -> rebindToView syms)
 
-    filterSections.SelectionChanged.Add(fun _ ->
-        if filterSections.SelectedIndex >= 0 then
-            filterSections.SelectedIndex <- -1)
+    controls.filterSections.SelectionChanged.Add(fun e ->
+        if controls.filterSections.SelectedIndex >= 0 then
+            controls.filterSections.SelectedIndex <- -1)
 
     for section in syms |> Seq.map (fun (_, section, _) -> section) |> set do
         let sectionName = if section = "" then "<other>" else section
         let item = CheckBox(Content = section, IsChecked = Nullable<bool>(true), Tag = section)
         item.Unchecked.Add(fun _ -> rebindToView syms)
         item.Checked.Add(fun _ -> rebindToView syms)
-        filterSections.Items.Add(item) |> ignore
+        controls.filterSections.Items.Add(item) |> ignore
 
 app.Startup.Add(fun _ ->
     let path =
@@ -107,19 +112,20 @@ app.Startup.Add(fun _ ->
             Environment.GetCommandLineArgs().[1]
         else
             let dlg = OpenFileDialog(DefaultExt = ".elf", Filter = "ELF files (.elf)|*.elf")
-            let res = dlg.ShowDialog(w)
+            let res = dlg.ShowDialog(window)
             if res.HasValue && res.Value then
                 dlg.FileName
             else
                 exit 0
 
-    treeView.ItemsSource <- [sprintf "Loading %s..." path]
+    controls.labelLoading.Content <- sprintf "Loading %s..." path
 
     async {
         let ess = ElfSymbolSource(path, fun key -> Map.find key config) :> ISymbolSource
         let symbols = ess.Symbols |> Seq.map (fun s -> int s.size, s.section, s.name) |> Seq.toArray
         do! Async.SwitchToContext context
         bindToView symbols
+        controls.tabTreeView.IsSelected <- true
     } |> Async.Start)
 
-app.Run(w) |> ignore
+app.Run(window) |> ignore
