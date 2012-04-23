65a7c6e8fb5a9e3dba7e39c3da9c05257f4f73b4
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-04-23 00:09:55 -0700

    Implemented window placement save/restore

--- src/settings.fs
+++ src/settings.fs
@@ -1,14 +1,38 @@
 namespace UI
 
+#nowarn "9" // StructLayout produces 'Uses of this construct may result in the generation of unverifiable .NET IL code' warning
+
 open System
 open System.Collections.Concurrent
-open System.Collections.Generic
 open System.ComponentModel
 open System.IO
+open System.Runtime.InteropServices
 open System.Windows
 open System.Windows.Data
 open System.Windows.Markup
+open System.Windows.Interop
 open System.Xml
+
+module private Win32 =
+    let SW_SHOWNORMAL = 1
+    let SW_SHOWMINIMIZED = 2
+
+    [<Struct; StructLayout(LayoutKind.Sequential)>]
+    type WINDOWPLACEMENT =
+        val mutable length: int
+        val mutable flags: int
+        val mutable showCmd: int
+        val mutable minPositionX: int
+        val mutable minPositionY: int
+        val mutable maxPositionX: int
+        val mutable maxPositionY: int
+        val mutable normalPositionLeft: int
+        val mutable normalPositionTop: int
+        val mutable normalPositionRight: int
+        val mutable normalPositionBottom: int
+
+    [<DllImport("user32")>] extern bool SetWindowPlacement(nativeint handle, [<In>] WINDOWPLACEMENT& placement)
+    [<DllImport("user32")>] extern bool GetWindowPlacement(nativeint handle, WINDOWPLACEMENT& placement)
 
 module Settings =
     type Item() =
@@ -79,3 +103,54 @@ type AutoSave(defaultValue) =
 
             Binding(Source = item, Path = PropertyPath("Value"), Mode = BindingMode.TwoWay).ProvideValue(sp)
         | _ -> null
+
+type Window() =
+    static let loadHandler = EventHandler(fun e args ->
+        let window = e :?> Windows.Window
+        let handle = WindowInteropHelper(window).Handle
+        let placement = Settings.current.[sprintf "%s/Placement" window.Name].Value :?> string
+        let pldata = if placement = null then [||] else placement.Split(null) |> Array.map (Int32.TryParse) |> Array.choose (fun (r, v) -> if r then Some v else None)
+
+        match pldata with
+        | [|flags; showCmd; minPositionX; minPositionY; maxPositionX; maxPositionY; normalPositionLeft; normalPositionTop; normalPositionRight; normalPositionBottom|] ->
+            let showCmdFix = if showCmd = Win32.SW_SHOWMINIMIZED then Win32.SW_SHOWNORMAL else showCmd
+            let mutable pl =
+                Win32.WINDOWPLACEMENT(
+                    length = Marshal.SizeOf(typeof<Win32.WINDOWPLACEMENT>),
+                    flags = flags, showCmd = showCmdFix,
+                    minPositionX = minPositionX, minPositionY = minPositionY,
+                    maxPositionX = maxPositionX, maxPositionY = maxPositionY,
+                    normalPositionLeft = normalPositionLeft, normalPositionTop = normalPositionTop,
+                    normalPositionRight = normalPositionRight, normalPositionBottom = normalPositionBottom)
+            Win32.SetWindowPlacement(handle, &pl) |> ignore
+        | _ -> ())
+        
+    static let saveHandler = CancelEventHandler(fun e args ->
+        let window = e :?> Windows.Window
+        let handle = WindowInteropHelper(window).Handle
+        let mutable pl = Win32.WINDOWPLACEMENT(length = Marshal.SizeOf(typeof<Win32.WINDOWPLACEMENT>))
+        if Win32.GetWindowPlacement(handle, &pl) then
+            Settings.current.[sprintf "%s/Placement" window.Name].Value <-
+                sprintf "%d %d %d %d %d %d %d %d %d %d" pl.flags pl.showCmd pl.minPositionX pl.minPositionY pl.maxPositionX pl.maxPositionY
+                    pl.normalPositionLeft pl.normalPositionTop pl.normalPositionRight pl.normalPositionBottom)
+        
+    static let autoSavePlacement =
+        DependencyProperty.RegisterAttached("AutoSavePlacement",
+            typeof<bool>, typeof<Windows.Window>, PropertyMetadata(false))
+
+    static member GetAutoSavePlacement(window: Windows.Window) =
+        window.GetValue(autoSavePlacement) :?> bool
+
+    static member SetAutoSavePlacement(window: Windows.Window, value: bool) =
+        if value <> Window.GetAutoSavePlacement window then
+            if value then
+                if String.IsNullOrEmpty(window.Name) then
+                    failwith "AutoSave properties require parent element to have a name"
+
+                window.SourceInitialized.AddHandler(loadHandler)
+                window.Closing.AddHandler(saveHandler)
+            else
+                window.SourceInitialized.RemoveHandler(loadHandler)
+                window.Closing.RemoveHandler(saveHandler)
+
+            window.SetValue(autoSavePlacement, value)
