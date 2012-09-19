a48ea0e42dbafe8152f5adea293b036d02bc0d26
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-09-18 21:47:12 -0700

    Restore EXE support.
    Executable support was disabled because of issues with preloading/prefetching -
    we really need the PDB path. What we can easily do is to try opening a session
    for the executable (this is fast), and then get the PDB file path from global
    symbol - and use PdbSymbolSource that does prefetch/preload optimizations.

--- src/symbols/exesymbolsource.fs
+++ src/symbols/exesymbolsource.fs
@@ -2,11 +2,17 @@ namespace Symbols
 
 open Dia2Lib
 
-type ExeSymbolSource(path) =
-    let source = DiaSymbolSource.CreateSource ()
-    do source.loadDataForExe(path, null, null)
+type ExeSymbolSource(path, preload) =
+    let pdbpath =
+        let source = DiaSymbolSource.CreateSource ()
+        do source.loadDataForExe(path, null, null)
 
-    let ss = DiaSymbolSource(source) :> ISymbolSource
+        // we can't use the session as is since we have to optimize I/O and/or preload PDB data
+        // so let's grab the PDB filename and use it in PdbSymbolSource
+        let session = source.openSession()
+        session.globalScope.symbolsFileName
+
+    let ss = PdbSymbolSource(pdbpath, preload) :> ISymbolSource
 
     interface ISymbolSource with
         member this.Sections = ss.Sections
